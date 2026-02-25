25e26439730307e97b46741280c6c102e43172c9
OmDongaonkar03 <officialom2006@gmail.com>
2026-02-25 11:57:30 +0530

    chore : add env example files

---
+++ authorization/.env.example.php
@@ -0,0 +1,8 @@
+<?php
+    define("DB_HOST", "your_db_host");
+    define("DB_USER", "your_db_user");
+    define("DB_PASS", "your_db_pass");
+    define("DB_NAME", "your_db_name");
+
+    define("APP_URL", "https://your_domain_url");
+?>
